d7c0dd8e16fc2568a6e67497d10c6b5cf9a8d8f9
nii238686 <nii238686@gmail.com>
2023-08-02 13:55:09 +0100

    first commit

---
+++ src/Herosection.jsx
@@ -0,0 +1,10 @@
+export const Hero = () => {
+    return<>
+    <img src="https://images.unsplash.com/photo-1682687981907-170c006e3744?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80" alt="" />
+    <h1>What do you think</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam 
+        obcaecati cupiditate necessitatibus quidem impedit rerum, ipsam quis nemo.
+         Molestias voluptas ratione excepturi, 
+        hic id laboriosam tempore blanditiis animi architecto perferendis!</p>
+    </>
+}
